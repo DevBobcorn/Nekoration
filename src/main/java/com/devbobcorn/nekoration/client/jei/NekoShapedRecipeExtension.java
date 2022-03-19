fd7e2f624afb200eac513f7975745f9dcca27ad8
Tasuku Bobcorn <devbobcorn@outlook.com>
2022-03-19 21:49:09 +0800

    update obsolete JEI code

--- src/main/java/com/devbobcorn/nekoration/client/jei/NekoShapedRecipeExtension.java
+++ src/main/java/com/devbobcorn/nekoration/client/jei/NekoShapedRecipeExtension.java
@@ -11,7 +11,6 @@ import mezz.jei.api.gui.ingredient.ICraftingGridHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategoryExtension;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.common.util.Size2i;
 
 public class NekoShapedRecipeExtension implements ICraftingCategoryExtension {
     public static final ResourceLocation NAME = new ResourceLocation(Nekoration.MODID, "neko_crafting_shaped");
@@ -32,8 +31,13 @@ public class NekoShapedRecipeExtension implements ICraftingCategoryExtension {
         craftingGridHelper.setOutputs(builder, VanillaTypes.ITEM, recipe.getOutputs());
     }
 
-    @Nullable
-	public Size2i getSize() {
-		return new Size2i(recipe.getWidth(), recipe.getHeight());
-	}
+    @Override
+    public int getWidth() {
+        return recipe.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return recipe.getHeight();
+    }
 }
