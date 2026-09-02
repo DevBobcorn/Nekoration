7cb56ca787172cc40c977f6b4fa40ad7d4aa24a4
Tasuku Bobcorn <devbobcorn@outlook.com>
2026-09-02 12:03:01 +0800

    :adhesive_bandage: Update shapes for Awning and Short Awning blocks

--- src/main/java/io/devbobcorn/nekoration/blocks/AwningBlock.java
+++ src/main/java/io/devbobcorn/nekoration/blocks/AwningBlock.java
@@ -16,13 +16,34 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 /** Dyeable awning with an axe-toggleable end cap. */
 public class AwningBlock extends DyeableHorizontalBlock {
     public static final BooleanProperty IS_END = BlockStateProperties.BOTTOM;
     public static final MapCodec<AwningBlock> CODEC = simpleCodec(AwningBlock::new);
-    private static final VoxelShape SHAPE = Block.box(0.1D, 0.1D, 0.1D, 15.9D, 15.9D, 15.9D);
+
+    private static final VoxelShape SHAPE_NORTH = Shapes.or(
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 4.0D),
+            Block.box(0.0D, 4.0D, 4.0D, 16.0D, 8.0D, 8.0D),
+            Block.box(0.0D, 8.0D, 8.0D, 16.0D, 12.0D, 12.0D),
+            Block.box(0.0D, 12.0D, 12.0D, 16.0D, 16.0D, 16.0D));
+    private static final VoxelShape SHAPE_SOUTH = Shapes.or(
+            Block.box(0.0D, 0.0D, 12.0D, 16.0D, 4.0D, 16.0D),
+            Block.box(0.0D, 4.0D, 8.0D, 16.0D, 8.0D, 12.0D),
+            Block.box(0.0D, 8.0D, 4.0D, 16.0D, 12.0D, 8.0D),
+            Block.box(0.0D, 12.0D, 0.0D, 16.0D, 16.0D, 4.0D));
+    private static final VoxelShape SHAPE_EAST = Shapes.or(
+            Block.box(12.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D),
+            Block.box(8.0D, 4.0D, 0.0D, 12.0D, 8.0D, 16.0D),
+            Block.box(4.0D, 8.0D, 0.0D, 8.0D, 12.0D, 16.0D),
+            Block.box(0.0D, 12.0D, 0.0D, 4.0D, 16.0D, 16.0D));
+    private static final VoxelShape SHAPE_WEST = Shapes.or(
+            Block.box(0.0D, 0.0D, 0.0D, 4.0D, 4.0D, 16.0D),
+            Block.box(4.0D, 4.0D, 0.0D, 8.0D, 8.0D, 16.0D),
+            Block.box(8.0D, 8.0D, 0.0D, 12.0D, 12.0D, 16.0D),
+            Block.box(12.0D, 12.0D, 0.0D, 16.0D, 16.0D, 16.0D));
 
     public AwningBlock(Properties properties) {
         super(properties);
@@ -55,6 +76,15 @@ public class AwningBlock extends DyeableHorizontalBlock {
 
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return SHAPE;
+        switch (state.getValue(FACING)) {
+            case SOUTH:
+                return SHAPE_SOUTH;
+            case EAST:
+                return SHAPE_EAST;
+            case WEST:
+                return SHAPE_WEST;
+            default:
+                return SHAPE_NORTH;
+        }
     }
 }
--- src/main/java/io/devbobcorn/nekoration/blocks/ShortAwningBlock.java
+++ src/main/java/io/devbobcorn/nekoration/blocks/ShortAwningBlock.java
@@ -7,12 +7,37 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 /** Dyeable short awning. */
 public class ShortAwningBlock extends DyeableHorizontalBlock {
     public static final MapCodec<ShortAwningBlock> CODEC = simpleCodec(ShortAwningBlock::new);
-    private static final VoxelShape SHAPE = Block.box(0.1D, 2.0D, 0.1D, 15.9D, 15.9D, 15.9D);
+
+    private static final VoxelShape SHAPE_NORTH = Shapes.or(
+            Block.box(0.0D, 8.0D, 0.0D, 16.0D, 12.0D, 5.333333D),
+            Block.box(0.0D, 10.0D, 5.333333D, 16.0D, 14.0D, 10.666667D),
+            Block.box(0.0D, 12.0D, 10.666667D, 16.0D, 16.0D, 16.0D),
+            Block.box(1.0D, 4.0D, 2.0D, 2.0D, 11.0D, 16.0D),
+            Block.box(14.0D, 4.0D, 2.0D, 15.0D, 11.0D, 16.0D));
+    private static final VoxelShape SHAPE_SOUTH = Shapes.or(
+            Block.box(0.0D, 8.0D, 10.666667D, 16.0D, 12.0D, 16.0D),
+            Block.box(0.0D, 10.0D, 5.333333D, 16.0D, 14.0D, 10.666667D),
+            Block.box(0.0D, 12.0D, 0.0D, 16.0D, 16.0D, 5.333333D),
+            Block.box(14.0D, 4.0D, 0.0D, 15.0D, 11.0D, 14.0D),
+            Block.box(1.0D, 4.0D, 0.0D, 2.0D, 11.0D, 14.0D));
+    private static final VoxelShape SHAPE_EAST = Shapes.or(
+            Block.box(10.666667D, 8.0D, 0.0D, 16.0D, 12.0D, 16.0D),
+            Block.box(5.333333D, 10.0D, 0.0D, 10.666667D, 14.0D, 16.0D),
+            Block.box(0.0D, 12.0D, 0.0D, 5.333333D, 16.0D, 16.0D),
+            Block.box(0.0D, 4.0D, 1.0D, 14.0D, 11.0D, 2.0D),
+            Block.box(0.0D, 4.0D, 14.0D, 14.0D, 11.0D, 15.0D));
+    private static final VoxelShape SHAPE_WEST = Shapes.or(
+            Block.box(0.0D, 8.0D, 0.0D, 5.333333D, 12.0D, 16.0D),
+            Block.box(5.333333D, 10.0D, 0.0D, 10.666667D, 14.0D, 16.0D),
+            Block.box(10.666667D, 12.0D, 0.0D, 16.0D, 16.0D, 16.0D),
+            Block.box(2.0D, 4.0D, 14.0D, 16.0D, 11.0D, 15.0D),
+            Block.box(2.0D, 4.0D, 1.0D, 16.0D, 11.0D, 2.0D));
 
     public ShortAwningBlock(Properties properties) {
         super(properties);
@@ -25,6 +50,15 @@ public class ShortAwningBlock extends DyeableHorizontalBlock {
 
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return SHAPE;
+        switch (state.getValue(FACING)) {
+            case SOUTH:
+                return SHAPE_SOUTH;
+            case EAST:
+                return SHAPE_EAST;
+            case WEST:
+                return SHAPE_WEST;
+            default:
+                return SHAPE_NORTH;
+        }
     }
 }
