defaf89f2d1dadc8de8998900ed9c1bdf568908e
Tasuku Bobcorn <devbobcorn@outlook.com>
2026-09-19 18:17:29 +0800

    :bento: Update Cement textures

---
+++ common/src/main/resources/assets/nekoration/textures/block/cement/cop.sh
@@ -0,0 +1,10 @@
+cp ./cement_pillar_doric_t1.png ./cement_pillar_ionic_t1.png
+cp ./cement_pillar_doric_t1.png ./cement_pillar_corinthian_t1.png
+cp ./cement_pillar_doric_t0.png ./cement_pillar_ionic_t0.png
+cp ./cement_pillar_doric_t0.png ./cement_pillar_corinthian_t0.png
+
+cp ./cement_pillar_doric.png ./cement_pillar_doric_t2.png
+cp ./cement_pillar_ionic.png ./cement_pillar_ionic_t2.png
+cp ./cement_pillar_corinthian.png ./cement_pillar_corinthian_t2.png
+cp ./cement_pillar_simple.png ./cement_pillar_simple_t2.png
+
